56f7ac3a871c353eb7b78416b4d2f98072ee0d46
InYourSkin <32585506+InYourSkin@users.noreply.github.com>
2017-10-07 16:39:30 -0400

    finish mirror

---
+++ InYourSkin/Assets/InstantVR/Extensions/Traditional/IVR_TraditionalHand.cs
@@ -0,0 +1,74 @@
+﻿/* InstantVR Traditional hand
+ * author: Pascal Serrarens
+ * email: support@passervr.com
+ * version: 3.4.10
+ * date: June 19, 2016
+ * 
+ * - Removed unused MouseInput
+ */
+
+using UnityEngine;
+
+namespace IVR {
+
+    public class IVR_TraditionalHand : IVR_HandController {
+
+//        [HideInInspector]
+//        private IVR_HandMovementsBase handMovements;
+        [HideInInspector]
+        private IVR_AnimatorHand handAnimator;
+
+        [HideInInspector]
+        private bool joystick2available;
+        [HideInInspector]
+        private bool startBackAvailable;
+        [HideInInspector]
+        private bool fingerAxisAvailable, indexFingerAxisAvailable;
+
+        void Start() {
+        }
+
+        public override void StartController(InstantVR ivr) {
+            extension = ivr.GetComponent<IVR_Traditional>();
+            base.StartController(ivr);
+            tracking = true;
+
+            handAnimator = GetComponent<IVR_AnimatorHand>();
+        }
+
+        public override void UpdateController() {
+            if (enabled) {
+                if (handAnimator != null) {
+                    handAnimator.UpdateController();
+
+                    position = handAnimator.position;
+                    rotation = handAnimator.rotation;
+                    if (selected) {
+                        transform.position = position;
+                        transform.rotation = rotation;
+                    }
+                }
+            }
+        }
+
+        /*
+                // when we are holding an object alredy, we must explicitly drop it.
+                if (handMovements.grabbedObject != null && lastFingerInput <= 0) {
+                    lastFingerInput = fingerInput;
+                    fingerInput = 1;
+                } else {
+                    if (selected)
+                        lastFingerInput = fingerInput;
+                    else
+                        lastFingerInput = 0;
+                }
+         */
+
+
+        public override void OnTargetReset() {
+            if (selected) {
+                Calibrate(true);
+            }
+        }
+    }
+}
---
+++ InYourSkin/Assets/InstantVR/Readme.txt
@@ -0,0 +1,5 @@
+InstantVR
+
+Version 3.8.6
+• Fixed collisions with static objects
+• Fixed avatars without middle fingers
