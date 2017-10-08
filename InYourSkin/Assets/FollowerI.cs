1be37858ffdb11fb3ff04e8cb75921337c094eff
InYourSkin <32585506+InYourSkin@users.noreply.github.com>
2017-10-08 17:37:49 -0400

    Plugin Free version

---
+++ InYourSkin/Assets/FollowerI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerI : MonoBehaviour {
+	Transform target; //the enemy's target
+	int moveSpeed = 3; //move speed
+	int rotationSpeed = 3; //speed of turning
+	float range = 5f;
+	float range2 = 15f;
+	float stop = .5f;
+	Transform myTransform  ; //current transform data of this enemy
+
+	void Awake()
+	{
+		myTransform = transform; //cache transform data for easy access/preformance
+	}
+
+	void  Start()
+	{
+		target = GameObject.FindWithTag("Player").transform; //target the player
+
+	}
+
+	void Update () {
+		//rotate to look at the player
+		var distance = Vector3.Distance(myTransform.position, target.position);
+		if (distance<=range2 &&  distance>=range){
+			myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+				Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
+		}
+
+
+		else if(distance<=range && distance>stop){
+
+			//move towards the player
+			myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+				Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
+			myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+		}
+		else if (distance<=stop) {
+			myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+				Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
+		}
+	}
+}
+
---
+++ InYourSkin/Assets/main.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour {
+
+    public Texture2D texture1;
+    public Texture2D texture2;
+    public GameObject skin;
+    int textureIndex;
+    int filter = 0;
+    string[] TextureName = { "black-male-young", "asian-male-young", "caucasian-male-young" };
+    string[] AudioObjects = { "africa", "asian", "caucasian" };
+    GameObject sound_chRoom;
+    bool playFirstTime = true;
+    static int condition = -1;
+
+
+    // Use this for initialization
+    void Start () {
+
+
+        //controllerInput = Controllers.GetController(0);
+        skin = GameObject.Find("MakeHuman_simpleascottkMesh");
+
+    }
+
+    
+     
+
+        
+}
